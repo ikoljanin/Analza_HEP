60dd075de05606a8ec59d6a215954eb3523f8436
ikoljanin <ikoljanin@pmfst.hr>
2020-10-23 23:14:12 +0200

    Malo jednostavniji kod

--- V3/ElementaryParticle.cpp
+++ V3/ElementaryParticle.cpp
@@ -21,26 +21,16 @@
     
     //funkcija za unos podataka i računanje energije Higgsa
     // pokH->...ovakvo pozivanje funkcije pamti poziciju px_class itd. i zna da je to za Higgsa
-    double ElementaryParticle::Higgs_fourvector(double px, double py, double pz) 
+    double ElementaryParticle::fourvector(double px, double py, double pz) 
     {
+		//postavljanje količine gibanja
         px_class=px;
         py_class=py;
         pz_class=pz;
+		//energija
         E_class=sqrt(px_class*px_class+py_class*py_class+pz_class*pz_class+masa_class*masa_class);
-    }
-
-    //##funkcija za unos i računanje energije čestica na koje se raspadne ulazna čestica  
-    double ElementaryParticle::vrijednost() //f-ja ne prima ništa jer sve šta koristi je globalno definirano
-    {
-        E_class=sqrt(px_class*px_class+py_class*py_class+pz_class*pz_class+masa_class*masa_class);
-        return E_class;
-    }
-
-    //##funkcija za transverzanu količinu gibanja
-    double ElementaryParticle::trans()
-    {
-        pt_class=sqrt(px_class*px_class+py_class*py_class);
-        //cout<<ime_class << pt_class <<endl;
+		//transverzalna količina gibanja
+		pt_class=sqrt(px_class*px_class+py_class*py_class);
     }
 
     //##definicija override konstruktora (korisno za pozivanje čestica na koje se raspada ulazna čestica)
@@ -69,57 +59,20 @@
                 c2->masa_class=80.38;
                 c1->ime_class="W";
                 c2->ime_class="W";
-                //četverovektor prve čestice raspada
-                c1->px_class=(rand()%100)*(1.0)/100*px_class;
-                c1->py_class=(rand()%100)*(1.0)/100*py_class;
-                c1->pz_class=(rand()%100)*(1.0)/100*pz_class;
-				c1->trans();
-                c1->vrijednost();
-                //četverovektor druge čestice raspada
-                c2->px_class=px_class-c1->px_class;
-                c2->py_class=py_class-c1->py_class;
-                c2->pz_class=pz_class-c1->pz_class;
-				c2->trans();
-                c2->vrijednost();
             }
             else if(214<r && r<278)//raspad na 2 tau lepton
             {
                 c1->masa_class=1.776;
                 c2->masa_class=1.776;
                 c1->ime_class="T";
-                c2->ime_class="T";
-                //četverovektor prve čestice raspada
-                c1->px_class=(rand()%100)*(1.0)/100*px_class;
-                c1->py_class=(rand()%100)*(1.0)/100*py_class;
-                c1->pz_class=(rand()%100)*(1.0)/100*pz_class;
-				c1->trans();
-                c1->vrijednost();
-                //četverovektor druge čestice raspada
-                c2->px_class=px_class-c1->px_class;
-                c2->py_class=py_class-c1->py_class;
-                c2->pz_class=pz_class-c1->pz_class;
-				c2->trans();
-                c2->vrijednost();
-
+                c2->ime_class="T";   
             }
             else if(278<r && r<304)//raspad na 2 Z bozon
             {  
                 c1->masa_class=91.19;
                 c2->masa_class=91.19;
                 c1->ime_class="Z";
-                c2->ime_class="Z";
-                //četverovektor prve čestice raspada
-                c1->px_class=(rand()%100)*(1.0)/100*px_class;
-                c1->py_class=(rand()%100)*(1.0)/100*py_class;
-                c1->pz_class=(rand()%100)*(1.0)/100*pz_class;
-				c1->trans();
-                c1->vrijednost();
-                //četverovektor druge čestice raspada
-                c2->px_class=px_class-c1->px_class;
-                c2->py_class=py_class-c1->py_class;
-                c2->pz_class=pz_class-c1->pz_class;
-				c2->trans();
-                c2->vrijednost();
+                c2->ime_class="Z";   
             }
             else if(r>304)//raspad na 2 b kvarkove
             {
@@ -127,22 +80,14 @@
                 c2->masa_class=4.18;
                 c1->ime_class="b";
                 c2->ime_class="b";
-                //četverovektor prve čestice raspada
-                c1->px_class=(rand()%100)*(1.0)/100*px_class;
-                c1->py_class=(rand()%100)*(1.0)/100*py_class;
-                c1->pz_class=(rand()%100)*(1.0)/100*pz_class;
-				c1->trans();
-                c1->vrijednost();
-                //četverovektor druge čestice raspada
-                c2->px_class=px_class-c1->px_class;
-                c2->py_class=py_class-c1->py_class;
-                c2->pz_class=pz_class-c1->pz_class;
-				c2->trans();
-                c2->vrijednost(); 
             }
-            //cout<<c<<"  "<< ime_class<<"  "<< px_class<<" "<<py_class<<" "<<pz_class<<" "<<E_class<<endl; 
-            //cout<<c<<"\t"<< c1->ime_class<<"\t"<<  c1->px_class<<"\t"<<c1->py_class<<"\t"<<c1->pz_class<<"\t"<<c1->E_class<<endl;  
-            //cout<<c<<"\t"<< c2->ime_class<<"\t"<<  c2->px_class<<"\t"<<c2->py_class<<"\t"<<c2->pz_class<<"\t"<<c2->E_class<<endl;
+			//totalno nepotrebno ovaj korak ponavljati u svakom if
+			double p1x=(rand()%100)*(1.0)/100*px_class;
+			double p1y=(rand()%100)*(1.0)/100*py_class;
+			double p1z=(rand()%100)*(1.0)/100*pz_class;
+			//funkcija fourvector prima vrijednosti komponenti količine gibanja, postavi količinu gibanja i energiju na nešto
+            c1->fourvector(p1x,p1y,p1z);
+			c2->fourvector(px_class-c1->px_class,py_class-c1->py_class,pz_class-c1->pz_class);
             ofstream file;
             file.open ("rezultati.txt",ios_base::app); //svaki put dodajem podatke u analysis.txt pa stavim app (append) jer inace izbrise podatke za prethodni Higgsov bozon
             file <<c<<"\t"<< c1->ime_class<<"\t"<<  c1->px_class<<"\t"<<c1->py_class<<"\t"<<c1->pz_class<<"\t"<<c1->pt_class<<"\t"<<c1->E_class<<endl;
